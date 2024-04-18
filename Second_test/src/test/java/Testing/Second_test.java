dae38e817ba1e09f31302648a1af24ec4181660b
Любимов Дмитрий <142087671+LyubimovDS@users.noreply.github.com>
2024-04-18 11:24:30 +0300

    Add files via upload

---
+++ Second_test/src/test/java/Testing/Second_test.java
@@ -0,0 +1,29 @@
+package Testing;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
+
+public class Second_test {
+    @Test
+    public void check_creating_execution_and_deleting_task() {
+        open("https://webdriveruniversity.com/To-Do-List/index.html");
+
+
+        $("input").val("Сделать что-то").pressEnter();
+
+
+        $x("//li[text()=\" Сделать что-то\"]").shouldBe(exist).click();
+
+
+        $x("//li[@class=\"completed\"]").shouldBe(exist);
+
+
+        $x("//li[text()=\" Сделать что-то\"]//span").hover().click();
+
+
+        $(byXpath("//li[text()=\" Сделать что-то\"]")).shouldNotBe();
+    }
+}
