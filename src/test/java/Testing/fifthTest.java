27d705ee8e386cc8668731505638041744cd83e6
LyubimovDS <diman_47477@mail.ru>
2024-08-05 12:39:34 +0300

    fifthTest

--- src/test/java/Testing/fifthTest.java
+++ src/test/java/Testing/fifthTest.java
@@ -1,16 +1,19 @@
 package Testing;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import org.junit.jupiter.api.Test;
-
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-class Map {
+class fifthTest {
 
-    public static HashMap<String, List<String>> createReferenceMap(){
+    public HashMap<String, List<String>> createReferenceMap(){
 
         HashMap<String, List<String>> mainMap = new HashMap<>();
 
@@ -26,15 +29,23 @@ class Map {
     @Test
     public void workingWithAlerts() {
 
+
+        HashMap<String, List<String>> mainMap = createReferenceMap();
+
         open("https://demoqa.com/books");
 
-        Map map = new Map();
+        for (Map.Entry<String, List<String>> item : mainMap.entrySet()) {
 
-        for(map.mainMap.Entry<String, List<String>> item : map.mainMap.entrySet()){
+            String key = item.getKey();         // Получаем ключ
+            List<String> value = item.getValue();    // Получаем значение
 
-            String key = entry.getKey();         // Получаем ключ
-            Integer value = entry.getValue();    // Получаем значение
+            ElementsCollection cell = $$("[role='row']").findBy(text(key)).$$("[role='gridcell']");
+            String author = mainMap.get(key).get(0);
+            String publisher = mainMap.get(key).get(1);
 
 
+            cell.get(2).getText().equals(author);
+            cell.get(3).getText().equals(publisher);
+        }
     }
 }
