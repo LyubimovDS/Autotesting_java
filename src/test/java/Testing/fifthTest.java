9f8d4fb51d225b3981c9703b515335b3cbb596cc
lyubimovDS <lybimov@kinoplan.tech>
2024-11-14 09:49:48 +0300

    fifthTest with cucumber

--- src/test/java/Testing/fifthTest.java
+++ src/test/java/Testing/fifthTest.java
@@ -2,33 +2,48 @@ package Testing;
 
 
 import com.codeborne.selenide.ElementsCollection;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class fifthTest {
+public class fifthTest {
 
-    public HashMap<String, List<String>> createReferenceMap(){
+   @Given("Открыть страницу")
+    public void openSite(){open ("https://demoqa.com/webtables");}
 
-        HashMap<String, List<String>> mainMap = new HashMap<>();
+    @Given("Таблица с персоналом")
+    public void CheckTable(){$("[class='web-tables-wrapper']").shouldBe(exist);}
 
-        mainMap.put("Git Pocket Guide", List.of("Richard E. Silverman", "O'Reilly Media"));
-        mainMap.put("Learning JavaScript Design", List.of("Addy Osmani", "O'Reilly Media"));
-        mainMap.put("Designing Evolvable Web APIs with ASP.NET", List.of("Glenn Block et al.", "O'Reilly Media"));
-        mainMap.put("Speaking JavaScript", List.of("Axel Rauschmayer", "O'Reilly Media"));
+    @Then("В таблице должен быть персонал")
+    public void checkPersons(DataTable table){
+        List<List<String>> person = table.asLists(String.class);
 
-        return mainMap;
+        for (List<String> row : person){
+
+            String key = row.get(0);
+            ElementsCollection cell = $$("[role='row']").findBy(text(key)).$$("[role='gridcell']");
+            int i = 0;
+            for (String item: row){
+                assertEquals(item, cell.get(i).getText());
+                System.out.println(item + " = " + cell.get(i).getText());
+                i++;
+            }
+        }
     }
 
-
-    @Test
-    public void workingWithAlerts() {
+   /* public void workingWithAlerts() {
 
         open("https://demoqa.com/books");
 
@@ -37,6 +52,7 @@ class fifthTest {
             String key = item.getKey();                     // Получаем ключ
             String author = item.getValue().get(0);         // Получаем 1 значение листа
             String publisher = item.getValue().get(1);      // Получаем 2 значение листа
+
             ElementsCollection cell = $$("[role='row']").findBy(text(key)).$$("[role='gridcell']");
 
             assertEquals(author, cell.get(2).getText());
@@ -46,5 +62,5 @@ class fifthTest {
             System.out.println(publisher + " = " + cell.get(3).getText());
 
         }
-    }
+    }*/
 }
---
+++ src/test/resources/ListPersons.feature
@@ -0,0 +1,12 @@
+#language: ru
+
+ Функционал: Проверка содержания вебтаблицы
+   Сценарий: Открыть страницу
+     * Открыть страницу
+
+   Сценарий: Проверить содержание вебтаблицы
+     Дано Таблица с персоналом
+     Тогда В таблице должен быть персонал
+     |Cierra|Vega|39|cierra@example.com|10000|Insurance|
+     |Alden|Cantrell|45|alden@example.com|12000|Compliance|
+     |Kierra|Gentry|29|kierra@example.com|2000|Legal|
