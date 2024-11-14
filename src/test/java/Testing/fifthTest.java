5071d9b695812b20c828da1514ff01865629dd23
lyubimovDS <lybimov@kinoplan.tech>
2024-11-14 09:53:14 +0300

    fifthTest with cucumber removed everything unnecessary

--- src/test/java/Testing/fifthTest.java
+++ src/test/java/Testing/fifthTest.java
@@ -5,13 +5,9 @@ import com.codeborne.selenide.ElementsCollection;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.junit.jupiter.api.Test;
 
-import javax.swing.*;
-import java.lang.reflect.Type;
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.text;
@@ -20,47 +16,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class fifthTest {
 
-   @Given("Открыть страницу")
-    public void openSite(){open ("https://demoqa.com/webtables");}
+    @Given("Открыть страницу")
+    public void openSite() {
+        open("https://demoqa.com/webtables");
+    }
 
     @Given("Таблица с персоналом")
-    public void CheckTable(){$("[class='web-tables-wrapper']").shouldBe(exist);}
+    public void CheckTable() {
+        $("[class='web-tables-wrapper']").shouldBe(exist);
+    }
 
     @Then("В таблице должен быть персонал")
-    public void checkPersons(DataTable table){
+    public void checkPersons(DataTable table) {
         List<List<String>> person = table.asLists(String.class);
 
-        for (List<String> row : person){
+        for (List<String> row : person) {
 
             String key = row.get(0);
             ElementsCollection cell = $$("[role='row']").findBy(text(key)).$$("[role='gridcell']");
             int i = 0;
-            for (String item: row){
+            for (String item : row) {
                 assertEquals(item, cell.get(i).getText());
                 System.out.println(item + " = " + cell.get(i).getText());
                 i++;
             }
         }
     }
-
-   /* public void workingWithAlerts() {
-
-        open("https://demoqa.com/books");
-
-        for (Map.Entry<String, List<String>> item : createReferenceMap().entrySet()) {
-
-            String key = item.getKey();                     // Получаем ключ
-            String author = item.getValue().get(0);         // Получаем 1 значение листа
-            String publisher = item.getValue().get(1);      // Получаем 2 значение листа
-
-            ElementsCollection cell = $$("[role='row']").findBy(text(key)).$$("[role='gridcell']");
-
-            assertEquals(author, cell.get(2).getText());
-            assertEquals(publisher, cell.get(3).getText());
-
-            System.out.println(author + " = " + cell.get(2).getText());
-            System.out.println(publisher + " = " + cell.get(3).getText());
-
-        }
-    }*/
 }
