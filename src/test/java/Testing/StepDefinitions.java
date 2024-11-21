34f7a2bd56e176f866050d34a35f91794478f3db
lyubimovDS <lybimov@kinoplan.tech>
2024-11-21 08:22:04 +0300

    Написал сообщения об ошибках, оптимизировал код

--- src/test/java/Testing/StepDefinitions.java
+++ src/test/java/Testing/StepDefinitions.java
@@ -4,7 +4,7 @@ import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.ru.И;
+
 
 public class StepDefinitions {
     fifthTest test = new fifthTest();
